349f75478cd669028bddf1938be3498ba7e5d935
Luiza Camargo <luiza.camargo@euax.com.br>
2022-04-05 13:51:38 -0300

    Adicionando javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+function teste () {
+    const name = document.getElementById('name').value
+    const mail = document.getElementById('mail').value
+    const area = document.getElementById('area').value
+
+    return alert(`Wooooooooow \n \n Seja bem-vindo ${name} ao bootcamp da àrea de ${area}. \n\n Você se cadastrou com o e-mail \n ${mail}.`)
+}
